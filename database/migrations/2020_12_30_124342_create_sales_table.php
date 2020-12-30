523bf6a89677027617b214fa7154b2bb0db27a4e
RangelDheo <rangeldheo@gmail.com>
2020-12-30 10:06:12 -0300

    Criado recurso vendas

---
+++ database/migrations/2020_12_30_124342_create_sales_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->decimal('value',16,2)->default(0)->comment('Valor da venda');
+            $table->timestamp('on_date')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('Data de quando ocorreu a venda');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
