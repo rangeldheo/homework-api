417a39287ea04eb64052bdaea48d6a0626063699
RangelDheo <rangeldheo@gmail.com>
2020-12-30 10:12:24 -0300

    Cria factory vendas

--- database/factories/SaleFactory.php
+++ database/factories/SaleFactory.php
@@ -7,6 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Sale::class, function (Faker $faker) {
     return [
-        //
+        'user_id',
+        'value'=>$faker->randomFloat(2,10,5000),
+        'on_date'=>$faker->dateTimeBetween('-30 days','now')
     ];
 });
